fe75d860ccce7e177f845e54ce186522173f309a
KhanhNguyen2000 <116627856+KhanhNguyen2000@users.noreply.github.com>
2023-05-23 16:07:41 +0700

    commit

--- DancingGoatCore/Components/ViewComponents/Navigation/NavigationViewComponent.cs
+++ DancingGoatCore/Components/ViewComponents/Navigation/NavigationViewComponent.cs
@@ -1,5 +1,6 @@
-﻿using System.Linq;
-
+﻿using System.Collections.Generic;
+using System.Linq;
+using CMS.DocumentEngine;
 using DancingGoat.Models;
 
 using Kentico.Content.Web.Mvc;
@@ -20,13 +21,57 @@ namespace DancingGoat.Controllers
             this.pageUrlRetriever = pageUrlRetriever;
         }
 
+        public bool CheckRootParent(TreeNode node, string nameParent)
+        {
+            if(node.Parent == null && node.DocumentName == nameParent)
+            {
+                return true;
+            }
+            if (node.Parent == null && node.DocumentName != nameParent)
+            {
+                return false;
+            }
+            if (node.Parent.DocumentName != nameParent) {
+                return CheckRootParent(node.Parent, nameParent); 
+            }
+            return true;
+        }
 
-        public IViewComponentResult Invoke(bool footerNavigation)
+        public IViewComponentResult Invoke(bool footerNavigation, bool flag)
         {
             var viewName = footerNavigation ? "Footer" : "Menu";
             var menuItems = footerNavigation ? navigationRepository.GetFooterNavigationItems() : navigationRepository.GetMenuItems();
-            var menuItemsModel = menuItems.Select(menuItem => MenuItemViewModel.GetViewModel(menuItem, pageUrlRetriever));
+            HashSet<string> customers = new HashSet<string>();
+            List<TreeNode> lst = new List<TreeNode>();
+            
+            if(flag)
+            {
+                foreach (var menuItem in menuItems)
+                {
+                    bool rs = CheckRootParent(menuItem, "Khách hàng cá nhân");       
+                    if (rs)
+                    {
+                        lst.Add(menuItem);
+                    }
+                }
+            }
+            else
+            {
+                foreach (var menuItem in menuItems)
+                {
+                    bool rs = CheckRootParent(menuItem, "Khách hàng doanh nghiệp");
+                    if (rs)
+                    {
+                        lst.Add(menuItem);
+                    }
+                }
+            }
+            
 
+
+            var menuItemsModel = lst.Select(menuItem => MenuItemViewModel.GetViewModel(menuItem, pageUrlRetriever));
+            /*var menuItemsModel = menuItems.Select(menuItem => MenuItemViewModel.GetViewModel(menuItem, pageUrlRetriever));
+            */
             return View($"~/Components/ViewComponents/Navigation/{viewName}.cshtml", menuItemsModel);
         }
     }
--- DancingGoatCore/wwwroot/Scripts/menu.js
+++ DancingGoatCore/wwwroot/Scripts/menu.js
@@ -1,28 +1,53 @@
-const btnMenu = document.querySelector(".btn-menu")
+const btnToggle = document.querySelector(".btn-toggle")
+const btnExit = document.querySelector(".btn-exit")
 const listMenu = document.querySelector(".page-menu")
-console.log(btnMenu)
-btnMenu.addEventListener("click", (e) => {
 
-    listMenu.style.display = listMenu.style.display =="block" ? "none" : "block";
-    
-    console.log({ listMenu })
+btnToggle.addEventListener("click", (e) => {
+    listMenu.style.display = listMenu.style.display == "block" ? "none" : "block";
+    btnExit.style.display = "flex";
+    btnToggle.style.display = "none";
 })
+
+btnExit.addEventListener("click", (e) => {
+    listMenu.style.display = listMenu.style.display == "block" ? "none" : "block";
+    btnToggle.style.display = "flex";
+    btnExit.style.display = "none";
+})
+
+document.addEventListener('click', function (event) {
+    var isClickInside = btnToggle.contains(event.target);
+    var isClickInsideMenu = listMenu.contains(event.target);
+    if (isClickInside || isClickInsideMenu) {
+        console.log('Clicked INSIDE element')
+    }
+    else {
+        listMenu.style.display = "none";
+        btnToggle.style.display = "flex";
+        btnExit.style.display = "none";
+        console.log('Clicked OUTSIDE element')
+    }
+});
 
 const btnkhcn = document.querySelector(".btn-khcn")
 const btnkhdn = document.querySelector(".btn-khdn")
+const menuCustomerPersonal = document.querySelector(".menu-customer-personal")
+const menuCustomerBusiness = document.querySelector(".menu-customer-business")
 
 btnkhdn.addEventListener("click", (e) => {
-    console.log({btnkhdn})
     btnkhdn.style.background = "#005D98";
     btnkhdn.style.color = "#ffffff";
     btnkhcn.style.background = "#ffffff";
     btnkhcn.style.color = "black";
+    menuCustomerPersonal.style.display = "none";
+    menuCustomerBusiness.style.display = "flex";
 })
 
 btnkhcn.addEventListener("click", (e) => {
-    console.log({ btnkhdn })
     btnkhcn.style.background = "#005D98";
     btnkhdn.style.color = "black";
     btnkhdn.style.background = "#ffffff";
     btnkhcn.style.color = "#ffffff";
+    menuCustomerPersonal.style.display = "flex";
+    menuCustomerBusiness.style.display = "none";
 })
+
